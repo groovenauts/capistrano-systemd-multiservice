27b13001eded8e0081fbc6cf66a7e7492da49b36
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2016-12-08 20:14:57 +0900

    Added tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "rspec"
+require "mocha/api"
 require "capistrano/systemd/multiservice"
+
+RSpec.configure do |config|
+  config.raise_errors_for_deprecations!
+  config.mock_framework = :mocha
+  config.order = "random"
+end
