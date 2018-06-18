dd96490c2dae6ecf453f36160b07fccb9acde927
YAMADA Tsuyoshi <tsu-yamada@groovenauts.jp>
2018-06-19 02:06:29 +0900

    Defined Capistrano::Systemd::MultiService.new_service method

--- lib/capistrano/systemd/multiservice.rb
+++ lib/capistrano/systemd/multiservice.rb
@@ -4,6 +4,9 @@ require "capistrano/systemd/multiservice/system_service"
 module Capistrano
   module Systemd
     module MultiService
+      def self.new_service(app)
+        SystemService.new(app)
+      end
     end
   end
 end
--- spec/capistrano/systemd/multiservice_spec.rb
+++ spec/capistrano/systemd/multiservice_spec.rb
@@ -4,4 +4,15 @@ describe Capistrano::Systemd::MultiService do
   it "has a version number" do
     expect(Capistrano::Systemd::MultiService::VERSION).not_to be nil
   end
+
+  describe "Capistrano::Systemd::MultiService.new_service" do
+    context "with 'example' argument" do
+      subject {
+        Capistrano::Systemd::MultiService.new_service('example')
+      }
+      it do
+        expect(subject).to be_an_instance_of(Capistrano::Systemd::MultiService::SystemService).and have_attributes(app: "example")
+      end
+    end
+  end
 end
